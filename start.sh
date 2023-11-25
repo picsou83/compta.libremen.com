4fc8675a4c93a85da509cb91721d0f1f281750d6
picsou83 <34648108+picsou83@users.noreply.github.com>
2023-11-25 01:15:46 +0100

    MAJ v1.108

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-echo "modification des droits postgres"
-sudo chown -R postgres /var/lib/postgresql/ 
+echo "Extraction du fichier de mise à jour"
+tar -xpzf /tmp/comptalibre-server.tar.gz -C / --keep-newer-files
 
-echo "modification des droits apache"
+echo "Modification des droits postgres"
+sudo chown -R postgres /var/lib/postgresql/
+
+echo "Modification des droits apache"
 sudo chown -R www-data:www-data /var/www/
 sudo chmod 755 -R /var/www/
